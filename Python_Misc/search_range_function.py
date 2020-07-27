14c20ebe983ed87d90386b583285aaecc2435c66
macatak <mdmclauchlin@gmail.com>
2020-07-27 10:51:29 -0500

    add search_range_function.py
    Simple Python Function to perform a search

---
+++ Python_Misc/search_range_function.py
@@ -0,0 +1,25 @@
+#!/bin/python3
+
+'''
+search over a range as a function
+winlogbeat* event.code 4769 now-1d now
+
+'''
+
+# imports
+from elasticsearch import Elasticsearch
+from elasticsearch_dsl import Search
+
+def es_search(index,field,search_value,start_time,end_time):
+    # return the number of hits
+    es_search = Search(using=es_conn1, index=index) \
+    .filter('range' ,  **{'@timestamp': {'gte': start_time , 'lt':end_time}}) \
+    .query({"match": {field:search_value}})
+    return es_search.count()
+
+es_conn1 = Elasticsearch([{'host': '10.0.2.15', 'port': 9200}])
+
+
+if __name__ == '__main__':
+    test_search = es_search("messages","origin","kibana","now-1d","now")
+    print(test_search)
